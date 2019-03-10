67d47e443b50bca572f7475a7eba857f1fb0f2f9
Mattw46 <matthew_w46@yahoo.com.au>
2019-03-11 08:37:02 +1100

    Adding servlet and web page

---
+++ src/java/com/mattwdev/RoughRESTAPI.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mattwdev;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.*;
+
+/**
+ *
+ * @author neo
+ */
+public class RoughRESTAPI extends HttpServlet {
+
+    
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        JSONObject obj = new JSONObject();
+
+        obj.put("name","foo");
+        obj.put("num",new Integer(100));
+        obj.put("balance",new Double(1000.21));
+        obj.put("is_vip",new Boolean(true));
+
+        //StringWriter out = new StringWriter();
+        try {
+          obj.writeJSONString(out);
+        } catch (Exception e) {
+
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
